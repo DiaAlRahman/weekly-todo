53b0c3c1a231bb59e0c2510bfc7d9736a54a83ec
DiaAlRahman <127028600+DiaAlRahman@users.noreply.github.com>
2025-01-09 09:55:49 +0600

    add daily tasks backbone

--- src/daily_tasks.js
+++ src/daily_tasks.js
@@ -4,7 +4,7 @@ class Task {
     this.isDone = false;
   }
 
-  markdone() {
+  markDone() {
     this.isDone = true;
   }
 
@@ -19,13 +19,46 @@ class Task {
 }
 
 class DailyTasks {
+  constructor() {
+    this.tasks = []; // Array to store Task objects
+  }
 
+  addTask(task_name) {
+    const newTask = new Task(task_name)
+    this.tasks.push(newTask);
+  }
+
+  removeTask(taskName) {
+    this.tasks = this.tasks.filter((task) => task.name !== taskName);
+  }
+
+  listTasks() {
+    return this.tasks.map((task) => task.toString()).join("\n");
+  }
+
+  completeTask(taskName) {
+    const task = this.tasks.find((task) => task.name === taskName);
+    if (task) task.markDone();
+  }
+
+  incompleteTask(taskName) {
+    const task = this.tasks.find((task) => task.name === taskName);
+    if (task) task.markNotDone();
+  }
+
+  resetTasks() {
+    this.tasks.forEach((task) => task.markNotDone());
+  }
 }
 
-const workout = new Task('workout')
-console.log(workout.toString())
-workout.markdone()
-console.log(workout.toString())
+const dailyTasks = new DailyTasks();
+dailyTasks.addTask('workout');
+// console.log(dailyTasks.listTasks());
 
-const shower = new Task('shower')
-console.log(shower.toString())
+dailyTasks.completeTask('workout');
+// console.log(dailyTasks.listTasks());
+
+dailyTasks.addTask('shower');
+
+dailyTasks.resetTasks();
+console.log(dailyTasks.listTasks());
